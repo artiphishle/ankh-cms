b21e8f6933cb810173fdaa692f8dcc5614306205
artiphishle <fabio.gartenmann@protonmail.com>
2024-07-14 20:11:33 +0200

    Update things, too tired

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 #!/usr/bin/env node
-// export * from "./dist/types.d.ts";
-import {execSynt} from "child_process";
+export * from "./dist/types.d.ts";
+import {execSync} from "child_process";
 
 console.log("Generating app now...");
 console.log('argv:');
 process.argv.forEach(console.log);
-execSync(npx ankh-runner);
+(async ()=> {
+  try {
+    const res = await execSync("npx ankh-runner");
+    console.log(res);
+    process.exit(0);
+  } catch(e){
+    console.log(e);
+    process.exit(1)
+  }
+
+})();
