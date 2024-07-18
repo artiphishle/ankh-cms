fb3303ceecf10be4b0bb1c9b0264f18de8d3f459
artiphishle <fabio.gartenmann@protonmail.com>
2024-07-18 10:40:01 +0200

    Generate nested components

---
+++ lib/index.ts
@@ -0,0 +1,6 @@
+import {resolve} from "path";
+import {spawn} from "child_process";
+
+const child = spawn("node", [resolve(__dirname, "installer/next/runner.js"), __dirname], {stdio: "inherit"});
+
+child.on("close", (code) => console.log(`Script finished with code: ${code}`))
--- lib/installer/next/runner.ts
+++ lib/installer/next/runner.ts
@@ -1,75 +1,97 @@
 import {resolve} from "path";
 import {execSync} from 'child_process';
-import {copyFileSync, cpSync, existsSync, mkdirSync, readFileSync, rmdirSync, rmSync, writeFileSync} from "fs";
+import {copyFileSync, cpSync, existsSync, mkdirSync, readFileSync, rmSync, writeFileSync} from "fs";
+import { IAnkhCmsConfig } from "../../types";
 
 console.log("Generating app...");
 
 // Get the current directory name
-const cmsDir = process.argv[2];
+const cmsDir = process.argv[2]!;
 const outDir = process.cwd();
-const libDir = resolve(cmsDir,'lib/');
+const libDir = resolve(cmsDir,'../lib/');
+const tplDir = resolve(libDir, "templates/")
 const distDir = resolve(outDir, 'next/');
 const publicDir = resolve(distDir, 'public/');
-const pagesDir = resolve(distDir, "src/app/(pages)");
+const pagesDir = resolve(distDir, "src/app/(pages)/");
 const configFile = resolve(outDir, "config.json");
 
-function generatePage({c, p, t}: {c: string, p: Record<string,string>, t: string}){
-  const props = Object.keys(p).map((k: string)=> `${k}="${p[k]}"`);
-  const comp = `<${c} ${props.join(" ")}>test: target: ${t}</${c}>`;
-  
-  return `
-    import React from "react";
-    import {${c}} from "ankh-ui";
-
-    export default function Page(){
-      return (${comp});
-    }
-  `;
+interface IPage {
+  components: IUi[];
+  name: string;
 }
 
-if(!existsSync(configFile)) copyFileSync(resolve(libDir, "installer/next/templates/config.json"), configFile);
-const config = JSON.parse(readFileSync(configFile, "utf8"));
+interface IUi {
+  ui: string;
+  p: Record<string, string|number>,
+  uis?: IUi[];
+}
+function genUi({ui, p, uis}: IUi){
+  const props = Object.keys(p).map((k: string)=> `${k}="${p[k]}"`);
+  const subUis = uis?.map((subUi, i)=> genUi({ui: subUi.ui, p: subUi.p }))
+  const subbies = subUis ? subUis?.join("\n") : [];
+  const comp: string = !subbies?.length ? `<${ui} ${props.join(" ")} />` : `<${ui} ${props.join(" ")}>${subbies}</${ui}>`;
+  return comp;
+}
 
-// Clear
-console.log("1. Clearing");
-if(existsSync(distDir)) rmdirSync(distDir);
+function generatePage({name, components}: IPage){
+  const ui = genUi(components[0]!);
+
+  return `import {Grid, Html} from "ankh-ui";
+    export default function Page(){
+      return (${ui});}`;
+}
+
+// Read config or set a default one
+if(!existsSync(configFile)) copyFileSync(resolve(tplDir,"config.json"), configFile);
+const config: IAnkhCmsConfig = JSON.parse(readFileSync(configFile, "utf8"));
+
+
+/** 1. Prepare 6 Install Next.js */
+console.log("Preparing & Installing Next.js...");
+
+if(existsSync(distDir)) rmSync(distDir, {recursive: true, force: true});
 mkdirSync(distDir);
+console.log("✅ Cleared target dir");
 
-console.log("2. Installing");
-execSync(`npx create-next-app@latest ${distDir} --ts --app --src-dir --no-eslint --no-import-alias --no-tailwind`, {encoding: 'utf8'});
+execSync(`pnpm dlx create-next-app@latest ${distDir} --ts --app --src-dir --no-eslint --no-import-alias --no-tailwind`, {encoding: 'utf8'});
+console.log("✅ Next.js app installed");
 
-console.log("3. Patch next.config.mjs");
-cpSync(resolve(libDir, "installer/next/templates/next.config.mjs"), resolve(distDir, "next.config.mjs"));
+/** 2. Patch files */
+console.log("Patching files...");
 
-console.log("4. Patch public/");
+cpSync(resolve(tplDir, "next.config.mjs"), resolve(distDir, "next.config.mjs"));
+console.log("✅ Add redirect /home ot next.config.mjs");
+
 rmSync(publicDir, {recursive: true, force: true});
 mkdirSync(publicDir);
+console.log("✅ emptied public/ directory");
 
-console.log("5. Patch globals.css");
-cpSync(resolve(libDir, "installer/next/templates/globals.css"), resolve(distDir, "src/app/globals.css"));
+cpSync(resolve(tplDir, "globals.css"), resolve(distDir, "src/app/globals.css"));
+console.log("✅ Default globals.css");
 
-console.log("6. Patch root layout");
-cpSync(resolve(libDir, "installer/next/templates/layout.tsx"), resolve(distDir, "src/app/layout.tsx"));
+cpSync(resolve(tplDir, "layout.tsx"), resolve(distDir, "src/app/layout.tsx"));
+console.log("✅ Add RootLayout");
 
-console.log("7. Installing 'ankh-ui'");
-execSync(`cd ${distDir} && pnpm i ankh-ui@latest`);
+/*
+console.log("3. Installing 'ankh-ui'");
+const child = spawn("pnpm", ["install", "ankh-ui"], { cwd: distDir, stdio: "inherit"});
+child.on("error", console.log);
+child.on("message", console.log);
+child.on("close", console.log);
+*/
 
-console.log("8. Generate pages");
-const demoPage = `
-  import React from "react";
-  export default function DemoPage(){
-    return (<h1>DemoPage</h1>);
-  }
-`
+/** 3. Generate pages */
+console.log("Generating pages...");
+
 mkdirSync(pagesDir);
 config.pages?.forEach((page: any) => {
   mkdirSync(resolve(pagesDir, page.name));
-  const generatedPage = generatePage(page.components[0]);
+  const generatedPage = generatePage(page);
   writeFileSync(resolve(pagesDir, page.name, "page.tsx" ), generatedPage, {encoding: "utf8"});
 });
 
-console.log("9. Prettier formatting");
+/** 4. Formatting source files (prettier) */
 execSync(`prettier --write ${distDir}`);
+console.log("✅ Formatted source files (Prettier)");
 
-console.log("\n\nSuccess!\n\n");
-console.log("Run:", "cd next && pnpm run dev");
+console.log("\n\nDONE! Now run 'cd next && pnpm install ankh-ui@latest && pnpm run dev'");
