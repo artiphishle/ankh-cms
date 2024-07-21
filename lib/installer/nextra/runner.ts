09225216297132b45a6c476f781cbf8d3fe0d988
artiphishle <fabio.gartenmann@protonmail.com>
2024-07-21 03:26:49 +0200

    Update ankh-ui package

---
+++ lib/installer/nextra/runner.ts
@@ -0,0 +1,15 @@
+/**
+ * https://nextra.site/docs/custom-theme
+ */
+
+import { execSync } from "child_process";
+
+// Install Nextra
+// @todo dynamic theme from config
+execSync("pnpm i nextra nextra-theme-blog");
+
+// @todo Replace Next.js Config
+// ./next.config.mjs
+
+// @todo put config in root dir
+// ./nextra.config.jsx
